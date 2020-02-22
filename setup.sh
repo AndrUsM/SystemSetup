3c98700aa2292af146c41db4fe2ad9c8acb2c615
Andriy Karpenko <37944374+AndrUsM@users.noreply.github.com>
2020-02-22 22:13:18 +0200

    ubuntu 18.04 setup script

---
+++ setup.sh
@@ -0,0 +1,40 @@
+sudo apt install --reinstall intel-microcode
+sudo apt-get update && sudo apt dist upgrade && sudo apt-get autoremove && sudo apt-get autoclean && sudo apt-get -f install
+sudo apt install -y libc6:i386 libasound2:i386 libasound2-data:i386 libasound2-plugins:i386 libgtk2.0-0:i386 libsdl2-2.0-0:i386 libsdl2-image-2.0-0:i386 libfreetype6:i386 libcurl3:i386
+sudo apt-get update && sudo apt dist upgrade && sudo apt-get autoremove && sudo apt-get autoclean && sudo apt-get -f install
+sudo apt install -y ubuntu-restricted-extras libavcodec-extra browser-plugin-freshplayer-pepperflash build-essential libgtk2.0-dev cmake gparted
+sudo apt-get update && sudo apt dist upgrade && sudo apt-get autoremove && sudo apt-get autoclean && sudo apt-get -f install
+sudo apt install exfat-fuse exfat-utils -y
+sudo apt install -y p7zip-rar rar unrar unace arj cabextract -y
+sudo apt-get install -y  htop screenfetch gparted inxi tlp -y
+sudo apt-get install libreoffice-writer libreoffice-calc libreoffice-impress -y
+
+sudo apt-get install caffeine
+sudo apt-get install telegram-desktop
+sudo apt install -y qbittorrent*
+sudo apt install -y vlc
+sudo apt-get install synaptic
+
+curl -sL https://deb.nodesource.com/setup_10.x | sudo bash
+sudo apt update
+sudo apt -y install nodejs
+
+# gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
+sudo apt-get install pulseaudio pulseaudio-utils
+sudo apt-get install alsamixer
+sudo apt install cmus
+sudo apt-get install krita krita-data
+sudo apt-get install darktable
+#sudo apt-get install default-jdk && sudo apt-get install openjdk-11-jdk-headless && sudo apt-get install ecj && sudo apt-get install openjdk-8-jdk-headless
+sudo apt-get install mysql-server   
+sudo apt-get install gpick
+sudo apt-get install mongodb-clients mongodb
+
+sudo apt-get install mongodb
+sudo systemctl start mongodb
+sudo systemctl enable mongodb
+
+#sudo apt-get update;
+#sudo apt-get install ruby-full
+
+sudo apt-get update && sudo apt dist upgrade && sudo apt-get autoremove && sudo apt-get autoclean && sudo apt-get -f install && sudo apt-get install -f
